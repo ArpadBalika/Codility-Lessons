11a09d0e19a06e44aec13f21a8d129f5a907c21d
Arpad Balika <arpad.balika@outlook.com>
2020-06-03 09:59:59 +0200

    Simpler solution for BinaryGap

--- Codility_L1(Iterations)_BinaryGap.cpp
+++ Codility_L1(Iterations)_BinaryGap.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <algorithm>
 
+// Binary representation of num. (num >= 0)
 std::vector<bool> to_binary(int num) noexcept
 {
 	std::vector<bool> binary;
@@ -12,6 +13,7 @@ std::vector<bool> to_binary(int num) noexcept
 	return binary;
 }
 
+// Initial solution.
 int solution(int N) noexcept
 {
 	const auto binary = to_binary(N);
@@ -27,8 +29,8 @@ int solution(int N) noexcept
 		auto nextTrue = std::find(currentTrue + 1, iterEnd, true);
 		if (nextTrue == iterEnd) break;
 
-		const auto d = std::distance(currentTrue, nextTrue) - 1;
-		maxGap = std::max(maxGap, static_cast<size_t>(d));
+		const auto gap = std::distance(currentTrue, nextTrue) - 1;
+		maxGap = std::max(maxGap, static_cast<size_t>(gap));
 
 		iter = nextTrue;
 	}
@@ -36,11 +38,26 @@ int solution(int N) noexcept
 	return maxGap;
 }
 
-int main()
+// Simple solution without helper function, vectors and iterators.
+int simpleSolution(int N) noexcept
 {
-	unsigned X = 1041;
+	int maxGap = 0;
+	int prev = -1;
+	int current = 0;
 
-	auto result = solution(X);
+	while (N)
+	{
+		bool bit = N % 2;
+		N >>= 1;
 
-	return 0;
+		if (bit)
+		{
+			int gap;
+			prev == -1 ? gap = 0 : gap = current - prev - 1;
+			prev = current;
+			maxGap = std::max(maxGap, gap);
+		}
+		++current;
+	}
+	return maxGap;
 }
