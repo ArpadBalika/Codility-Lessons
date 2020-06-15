dec3d355f792c6eb3287d0455b9ede5097652ea9
Arpad Balika <arpad.balika@outlook.com>
2020-06-15 16:51:53 +0200

    Fish
    Recursive solution

---
+++ Codility_L7(Stacks and Queues)_Fish.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <stack>
+
+void bigFishEatsSmallFish(size_t i, std::stack<int>& aliveSize, std::stack<int>& aliveDir, std::vector<int>& A, std::vector<int>& B)
+{
+	if (aliveSize.empty() || aliveDir.top() == B[i] || aliveDir.top() == 0)
+	{
+		aliveSize.push(A[i]);
+		aliveDir.push(B[i]);
+	}
+	else if (aliveSize.top() < A[i])
+	{
+		aliveSize.pop();
+		aliveDir.pop();
+		bigFishEatsSmallFish(i, aliveSize, aliveDir, A, B);
+	}
+}
+
+// Recursive solution. Detected time complexity: O(N)
+int solution(std::vector<int>& A, std::vector<int>& B)
+{
+	std::stack<int> aliveSize;
+	std::stack<int> aliveDir;
+
+	for (size_t i = 0; i != A.size(); ++i)
+	{
+		bigFishEatsSmallFish(i, aliveSize, aliveDir, A, B);
+	}
+
+	return aliveSize.size();
+}
